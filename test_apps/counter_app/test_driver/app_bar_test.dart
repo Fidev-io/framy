48b27777aaf79e0f2d8878fe39686b17d6602109
MarcinusX <marcinszalek1@gmail.com>
2020-06-03 14:58:54 +0200

    Added screenshots to workflow

--- test_apps/counter_app/test_driver/app_bar_test.dart
+++ test_apps/counter_app/test_driver/app_bar_test.dart
@@ -1,22 +1,27 @@
 import 'package:flutter_driver/flutter_driver.dart';
+import 'package:ozzie/ozzie.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('App Bar', () {
     FlutterDriver driver;
+    Ozzie ozzie;
 
     setUpAll(() async {
       driver = await FlutterDriver.connect();
+      ozzie = Ozzie.initWith(driver, groupName: 'counter-app');
     });
 
     tearDownAll(() async {
       if (driver != null) {
         driver.close();
       }
+      ozzie.generateHtmlReport();
     });
 
     test('should be visible', () async {
       await driver.waitFor(find.byValueKey('FramyAppBar'));
+      await ozzie.takeScreenshot('appbar_is_visible');
     });
   });
 }
