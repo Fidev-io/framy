65e0ab60050ca977d068ce9d455987e23a8972cc
MarcinusX <marcinszalek1@gmail.com>
2020-06-12 14:17:18 +0200

    Removed top padding from AppBar page

--- framy_generator/lib/generator/app_bar_page_generator.dart
+++ framy_generator/lib/generator/app_bar_page_generator.dart
@@ -9,7 +9,6 @@ class FramyAppBarPage extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const SizedBox(height: 16),
           const FramyHeaderText('AppBar'),
           const SizedBox(height: 16),
           AppBar(
