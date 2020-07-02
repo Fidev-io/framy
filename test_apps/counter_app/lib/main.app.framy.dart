35ee352bf4bf7dfdc80a6b139e3aab63da09190c
monika.stobieniecka <mstobieniecka1@gmail.com>
2020-07-02 10:34:28 +0200

    Run generator after merging conflicts

--- test_apps/counter_app/lib/main.app.framy.dart
+++ test_apps/counter_app/lib/main.app.framy.dart
@@ -30,6 +30,7 @@ Route onGenerateRoute(RouteSettings settings) {
     '/appbar': FramyAppBarPage(),
     '/button': FramyButtonPage(),
     '/toggle': FramyTogglePage(),
+    '/textfield': FramyTextFieldPage(),
     '/CounterFAB': FramyCounterFABCustomPage(),
     '/CounterTitle': FramyCounterTitleCustomPage(),
   };
@@ -139,6 +140,12 @@ class FramyDrawer extends StatelessWidget {
                       title: Text('Toggle'),
                       onTap: () =>
                           Navigator.of(context).pushReplacementNamed('/toggle'),
+                    ),
+                    ListTile(
+                      leading: SizedBox.shrink(),
+                      title: Text('TextField'),
+                      onTap: () => Navigator.of(context)
+                          .pushReplacementNamed('/textfield'),
                     ),
                   ],
                 ),
@@ -413,72 +420,6 @@ class _FramyColorItem extends StatelessWidget {
   String _hex(int val) => val.toRadixString(16).padLeft(2, '0').toUpperCase();
 }
 
-class FramyTogglePage extends StatefulWidget {
-  const FramyTogglePage() : super(key: const Key('FramyTogglePage'));
-
-  @override
-  _FramyTogglePageState createState() => _FramyTogglePageState();
-}
-
-class _FramyTogglePageState extends State<FramyTogglePage> {
-  bool switchState = false;
-  List<bool> toggleState = [false, true, false];
-  bool checkboxState = false;
-  String radioState = 'Option A';
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const FramyHeaderText('Switch'),
-            Switch(
-              value: switchState,
-              onChanged: (bool value) => setState(() => switchState = value),
-            ),
-            const FramyHeaderText('Toggle buttons'),
-            const SizedBox(height: 8),
-            ToggleButtons(
-              onPressed: (index) =>
-                  setState(() => toggleState[index] = !toggleState[index]),
-              isSelected: toggleState,
-              children: [
-                Icon(Icons.format_bold),
-                Icon(Icons.format_italic),
-                Icon(Icons.format_align_center),
-              ],
-            ),
-            const SizedBox(height: 8),
-            const FramyHeaderText('Checkbox'),
-            CheckboxListTile(
-              controlAffinity: ListTileControlAffinity.leading,
-              onChanged: (bool value) => setState(() => checkboxState = value),
-              value: checkboxState,
-              title: Text('Option'),
-            ),
-            const FramyHeaderText('Radio'),
-            RadioListTile(
-              value: 'Option A',
-              onChanged: (system) => setState(() => radioState = system),
-              groupValue: radioState,
-              title: Text('Option A'),
-            ),
-            RadioListTile(
-              value: 'Option B',
-              onChanged: (system) => setState(() => radioState = system),
-              groupValue: radioState,
-              title: Text('Option B'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 class FramyHeaderText extends StatelessWidget {
   final String text;
 
@@ -690,6 +631,81 @@ class _FramyButtonPageState extends State<FramyButtonPage> {
         ),
       ),
     );
+  }
+}
+
+class FramyTogglePage extends StatefulWidget {
+  const FramyTogglePage() : super(key: const Key('FramyTogglePage'));
+
+  @override
+  _FramyTogglePageState createState() => _FramyTogglePageState();
+}
+
+class _FramyTogglePageState extends State<FramyTogglePage> {
+  bool switchState = false;
+  List<bool> toggleState = [false, true, false];
+  bool checkboxState = false;
+  String radioState = 'Option A';
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const FramyHeaderText('Switch'),
+            Switch(
+              value: switchState,
+              onChanged: (bool value) => setState(() => switchState = value),
+            ),
+            const FramyHeaderText('Toggle buttons'),
+            const SizedBox(height: 8),
+            ToggleButtons(
+              onPressed: (index) =>
+                  setState(() => toggleState[index] = !toggleState[index]),
+              isSelected: toggleState,
+              children: [
+                Icon(Icons.format_bold),
+                Icon(Icons.format_italic),
+                Icon(Icons.format_align_center),
+              ],
+            ),
+            const SizedBox(height: 8),
+            const FramyHeaderText('Checkbox'),
+            CheckboxListTile(
+              controlAffinity: ListTileControlAffinity.leading,
+              onChanged: (bool value) => setState(() => checkboxState = value),
+              value: checkboxState,
+              title: Text('Option'),
+            ),
+            const FramyHeaderText('Radio'),
+            RadioListTile(
+              value: 'Option A',
+              onChanged: (system) => setState(() => radioState = system),
+              groupValue: radioState,
+              title: Text('Option A'),
+            ),
+            RadioListTile(
+              value: 'Option B',
+              onChanged: (system) => setState(() => radioState = system),
+              groupValue: radioState,
+              title: Text('Option B'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FramyTextFieldPage extends StatelessWidget {
+  const FramyTextFieldPage() : super(key: const Key('FramyTextFieldPage'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
 
