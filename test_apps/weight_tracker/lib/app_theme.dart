794ed3722065ac1e55b94403c5a7cb17033dddf5
monika.stobieniecka <mstobieniecka1@gmail.com>
2020-07-07 10:14:24 +0200

    Fixed generating theme when multiple theme framy objects passed

--- test_apps/weight_tracker/lib/app_theme.dart
+++ test_apps/weight_tracker/lib/app_theme.dart
@@ -3,7 +3,7 @@ import 'package:framy_annotation/framy_annotation.dart';
 
 @framyTheme
 class AppTheme {
-  static final themeData = ThemeData(
+  static ThemeData get themeData => ThemeData(
     primarySwatch: Colors.green,
     visualDensity: VisualDensity.adaptivePlatformDensity,
   );
